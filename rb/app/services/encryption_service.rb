07d172bd9de576d2ec80e96712bb542e6e16dd2a
Amadeus Dabela Lanoa <amadeus.arjuna@gmail.com>
2024-10-10 13:08:00 -0300

    feature: create encryption service

---
+++ rb/app/services/encryption_service.rb
@@ -0,0 +1,52 @@
+#original code by @yukithm https://gist.github.com/yukithm/de7fcba1bea8a997554353e556031b51
+require "openssl"
+require "base64"
+
+class EncryptionService
+  CIPHER_ALGO = "AES-256-CBC"
+  SALT_SIZE = 8
+
+  def initialize(password, algo = CIPHER_ALGO, salt_size = SALT_SIZE)
+    @password = password
+    @algo = algo
+    @salt_size = salt_size
+  end
+
+  def cipher(data)
+    bin = encrypt(data)
+
+    Base64.strict_encode64(bin)
+  end
+
+  def decipher(data)
+    bin = Base64.strict_decode64(data)
+
+    decrypt(bin)
+  end
+
+  private
+
+  def encrypt(data)
+    salt = OpenSSL::Random.random_bytes(@salt_size)
+    cipher = OpenSSL::Cipher::Cipher.new(@algo)
+
+    cipher.encrypt
+    cipher.pkcs5_keyivgen(@password, salt, 1)
+    enc_data = cipher.update(data) + cipher.final
+
+    salt + enc_data
+  end
+
+  def decrypt(enc_data)
+    enc_data = enc_data.dup
+    enc_data.force_encoding("ASCII-8BIT")
+    salt = enc_data[0, @salt_size]
+    data = enc_data[@salt_size..-1]
+
+    cipher = OpenSSL::Cipher::Cipher.new(@algo)
+    cipher.decrypt
+    cipher.pkcs5_keyivgen(@password, salt, 1)
+    
+    cipher.update(data) + cipher.final
+  end
+end
---
+++ rb/spec/services/encryption_sevice_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe EncryptionService do
+  let(:password) { "password" }
+  let(:service) { described_class.new(password) }
+
+  describe '.cipher' do
+    it 'encrypts data' do
+      data = "sensitive"
+
+      encrypted_data = service.cipher(data)
+
+      expect(encrypted_data).not_to eq(data)
+      expect(encrypted_data).not_to be_nil
+    end
+  end
+
+  describe '.decipher' do
+    it 'decrypts data back to original' do
+      data = "sensitive"
+
+      encrypted_data = service.cipher(data)
+      decrypted_data = service.decipher(encrypted_data)
+
+      expect(decrypted_data).to eq(data)
+    end
+  end
+end
